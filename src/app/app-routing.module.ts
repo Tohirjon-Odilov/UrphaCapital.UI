914c070eee9aa04a552b92c2c0c78a6c729c41b5
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-08-18 17:09:26 +0500

    complete county

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RequestSendComponent } from './pages/request-send/request-send.component';
+import { CountyComponent } from './pages/county/county.component';
 
 const routes: Routes = [
   { path: '', title: 'OverView', component:HomeComponent},
-  { path: 'request', title: 'OverView', component:RequestSendComponent},
+  { path: 'request', title: 'Send Request', component:RequestSendComponent},
+  { path: 'county', title: 'County', component:CountyComponent},
+  { path: '**', title: 'OverView', component:HomeComponent},
 ];
 
 @NgModule({
