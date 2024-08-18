269a2819b9038cb81da44ead91d880e9afaf8fc7
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-08-18 17:34:29 +0500

    course-info complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RequestSendComponent } from './pages/request-send/request-send.component';
 import { CountyComponent } from './pages/county/county.component';
+import { CourseInfoComponent } from './pages/course-info/course-info.component';
 
 const routes: Routes = [
   { path: '', title: 'OverView', component:HomeComponent},
   { path: 'request', title: 'Send Request', component:RequestSendComponent},
   { path: 'county', title: 'County', component:CountyComponent},
+  { path: "course-info", title: "Course Info", component:CourseInfoComponent},
   { path: '**', title: 'OverView', component:HomeComponent},
 ];
 
