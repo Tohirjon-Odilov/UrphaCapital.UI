3fa50379af5fafda5c44d8e51637de4985960b64
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-08-29 09:46:20 +0500

    some

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../../../services/course-services/course.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  constructor(private _courseService: CourseService) { }
+  ngOnInit(): void {
+    this.getAllCourses();
+  }
 
+  getAllCourses() {
+    this._courseService.getCourses().subscribe(
+      (data) => {
+        console.log(data);
+      }
+    )
+  }
 }
--- src/app/pages/request-send/request-send.component.ts
+++ src/app/pages/request-send/request-send.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LessonService } from '../../../services/lesson-services/lesson.service';
 
 @Component({
   selector: 'app-request-send',
@@ -12,7 +13,39 @@ export class RequestSendComponent {
   email = ""
   courseType = ""
 
-  sendRequest(){
-    throw new Error("Not implemented tet;");
+  //create lesson ishladi.
+  name: string = 'Name';
+  courseId: number = 1;
+  video?: Blob;
+
+  constructor(private _service: LessonService) {
+    this.createLesson();
+  }
+
+  createLesson() {
+    const formData = new FormData();
+
+    if (this.name) {
+      formData.append('Name', this.name);
+    }
+
+    formData.append('CourseId', this.courseId.toString());
+
+    if (this.video) {
+      formData.append('Video', this.video);
+    }
+
+    this._service.createLesson(formData).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.error('Error creating lesson:', error);
+      }
+    );
+  }
+  //create lesson ishladi.
+  sendRequest() {
+
   }
 }
