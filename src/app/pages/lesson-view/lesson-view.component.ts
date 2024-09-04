aded109af8a6d493b309aa9414f786091bbffb71
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-09-04 21:57:20 +0500

    a

--- src/app/pages/lesson-view/lesson-view.component.ts
+++ src/app/pages/lesson-view/lesson-view.component.ts
@@ -10,59 +10,50 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrl: './lesson-view.component.scss'
 })
 export class LessonViewComponent implements OnInit {
+  lessonId?: string | number | null = 0;
+  lesson?: Lesson;
+  videoUrl: any;
 
+  constructor(
+    private _lessonService: LessonService,
+    private _router: Router,
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer
+  ) {}
 
-  constructor(private _lessonService: LessonService, private _router: Router, private route: ActivatedRoute, private santizier: DomSanitizer) {
-
-  }
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.lessonId = params.get('lessonId');
     });
     this.getLessonById();
     this.getVideo();
   }
 
-  lessonId?: string | number | null = 0;
-  lesson?: Lesson;
-  videoUrl: any;
-
   getLessonById() {
-    this._lessonService.getLessonById(+this.lessonId!).subscribe(
-      (data) => {
-        this.lesson = data
+    if (this.lessonId) {
+      this._lessonService.getLessonById(+this.lessonId).subscribe((data) => {
+        this.lesson = data;
         console.log(data);
-      }
-    )
+      });
+    }
   }
 
   getVideo() {
-    this._lessonService.getLessonVideo(1).subscribe(
-      (videoStream) => {
-        const url = window.URL.createObjectURL(videoStream);
-        
-        //const hashedUrl = this.hash(url, 123);
-  
-        this.videoUrl = this.santizier.bypassSecurityTrustUrl(url);
-  
-        setTimeout(() => {
-          const videoElement = document.querySelector('video');
-          videoElement?.addEventListener('contextmenu', (e) => e.preventDefault());
-        }, 1000);
-      }
-    )
-  }
-  
+    if (this.lessonId) {
+      this._lessonService.getLessonVideo(+this.lessonId).subscribe(
+        (videoStream) => {
+          const url = window.URL.createObjectURL(videoStream);
+          this.videoUrl = this.sanitizer.bypassSecurityTrustUrl(url);
 
-  hash(input: string, key: number): string {
-    let chars: string[] = input.split('');
-    for (let i = 0; i < chars.length; i++) {
-      chars[i] = String.fromCharCode(chars[i].charCodeAt(0) ^ key);
+          setTimeout(() => {
+            const videoElement = document.querySelector('video');
+            videoElement?.addEventListener('contextmenu', (e) => e.preventDefault());
+          }, 1000);
+        },
+        (error) => {
+          console.error('Error fetching video:', error);
+        }
+      );
     }
-    return chars.join('');
-  }
-
-  unhash(hashed: string, key: number): string {
-    return this.hash(hashed, key);
   }
 }
